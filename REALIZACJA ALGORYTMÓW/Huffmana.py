f50f6d7b4b724c4acc76ca2a1d0478ee4d2e1aa2
DorotaH <146991219+DorotaH@users.noreply.github.com>
2024-04-15 17:17:09 +0200

    Update Huffmana.py

--- REALIZACJA ALGORYTMÓW/Huffmana.py
+++ REALIZACJA ALGORYTMÓW/Huffmana.py
@@ -1,6 +1,8 @@
 text = input()
 length = len(text)
 frequency = {}
+
+#kodowanie
 for elem in text:
     if elem not in frequency.keys():
         frequency.update({elem:text.count(elem)/length})
@@ -19,5 +21,17 @@ print(code)
 coded_text = ''
 for letter in text:
     coded_text = f'{coded_text}{code[letter]}'
-
 print(coded_text)
+
+#dekodowanie
+extract=''
+uncoded_text = ''
+for elem in coded_text:
+    extract = f'{extract}{elem}'
+    if extract in code.values():
+        for item in code.items():
+            if item[1] == extract:
+                uncoded_text = f'{uncoded_text}{item[0]}'
+                extract = ''
+                break
+print(uncoded_text)
